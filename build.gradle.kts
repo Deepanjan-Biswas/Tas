38bd2579f0439ddecc8670bde67b1b318799f16e
Deepanjan-Biswas <deepanjanbiswas877@gmail.com>
2026-06-30 03:07:57 +0530

    Initial: Setup root Gradle configuration

---
+++ build.gradle.kts
@@ -0,0 +1,18 @@
+// Root build.gradle.kts
+plugins {
+    id("com.android.application") version "8.2.0" apply false
+    id("com.android.library") version "8.2.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.21" apply false
+    id("org.jetbrains.kotlin.jvm") version "1.9.21" apply false
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
